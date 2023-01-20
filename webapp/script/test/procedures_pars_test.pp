65fd51805c93a0987a99822969a5401331931b2c
Pavel <pav.trestik@gmail.com>
2023-01-20 21:34:57 +0100

    new: test for procedures

---
+++ webapp/script/test/procedures_pars_test.pp
@@ -0,0 +1,19 @@
+const a1 = 5;
+var resFloat1: float;
+
+procedure test;
+    begin
+    end;
+
+procedure float returnTest;
+    return 2.5;
+
+procedure paramSimpleTest(p1, p2: float, p3: boolean);
+    begin
+    end;
+
+begin
+    call test;
+    resFloat1 = call returnTest;
+    call paramSimpleTest(a1, 2.8, true);
+end.
